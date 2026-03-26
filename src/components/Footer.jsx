40db039ea73d128685df8fdefa3284ac242a4dee
Adniin <adniin@5lobes.com>
2026-03-26 19:07:32 +0530

    fix: update footer icons to match header icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,9 +30,12 @@ const partners = [
         label: 'LinkedIn',
         href: 'https://www.linkedin.com/company/5lobes-technologies-pvt-ltd/',
         icon: (
-            <svg className="w-4 h-4" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
-            </svg>
+            <img
+                src="https://customer-assets.emergentagent.com/job_33424166-e621-4602-bbca-dc46b9531d7e/artifacts/7jv8hnhp_image.png"
+                alt="LinkedIn"
+                className="w-5 h-5 object-contain rounded-sm"
+                loading="lazy"
+            />
         ),
     },
     {
@@ -42,7 +45,7 @@ const partners = [
             <img
                 src="https://customer-assets.emergentagent.com/job_33424166-e621-4602-bbca-dc46b9531d7e/artifacts/mn96lm9e_image.png"
                 alt="Odoo"
-                className="w-4 h-4 object-contain brightness-0 invert"
+                className="w-5 h-5 object-contain"
                 loading="lazy"
             />
         ),
@@ -54,7 +57,7 @@ const partners = [
             <img
                 src="https://customer-assets.emergentagent.com/job_33424166-e621-4602-bbca-dc46b9531d7e/artifacts/au29ykee_image.png"
                 alt="TalentCorner"
-                className="w-4 h-4 object-contain brightness-0 invert"
+                className="w-6 h-6 object-contain"
                 loading="lazy"
             />
         ),
